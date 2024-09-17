2bccd576b7696aa0db2394176e3cf2cc6504b571
Pethkar-Tushar-Narendra <pethkartusharnarendra@gmail.com>
2024-09-17 15:55:32 +0530

    Made dashboard listing

---
+++ Components/CardsContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+
+const CardsContainer = () => {
+  return (
+    <>
+      <div class="bg-white">
+        <Card />
+      </div>
+    </>
+  );
+};
+
+export default CardsContainer;
