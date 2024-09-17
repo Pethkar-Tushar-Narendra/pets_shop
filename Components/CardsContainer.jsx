5cdeb207cd1baaf6a19a6da00ee8f9e93a45f4e0
Pethkar-Tushar-Narendra <pethkartusharnarendra@gmail.com>
2024-09-17 15:58:07 +0530

    Added more products

--- Components/CardsContainer.jsx
+++ Components/CardsContainer.jsx
@@ -4,7 +4,9 @@ import Card from "./Card";
 const CardsContainer = () => {
   return (
     <>
-      <div class="bg-white">
+      <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <Card />
+        <Card />
         <Card />
       </div>
     </>
